e95c413ed1b728ed06380ca17e3f936692ddc957
augustosalazar <augustosalazar@uninorte.edu.co>
2018-09-21 18:16:09 -0500

    change dir

--- ServicioWeb/php/cargarjson2.php
+++ ServicioWeb/php/cargarjson2.php
@@ -3,10 +3,10 @@
         <title>Insertar</title>
     </head>
     <body>
-    <?php 
-        //Recibe el archivo a traves del metodo POST y lo guarda en la carpeta Json's ubicada dentro del volumen PHP 
-        move_uploaded_file($_FILES['archivo']['tmp_name'] , "Json's/" . $_FILES['archivo']['name']);
-        $jsonCont = file_get_contents("Json's/" . $_FILES['archivo']['name']);
+    <?php
+        //Recibe el archivo a traves del metodo POST y lo guarda en la carpeta Json's ubicada dentro del volumen PHP
+        move_uploaded_file($_FILES['archivo']['tmp_name'] , "uploads/" . $_FILES['archivo']['name']);
+        $jsonCont = file_get_contents("uploads/" . $_FILES['archivo']['name']);
         $content = json_decode($jsonCont);
     ?>
     <h2 aling="center"> ¡Guardado Exitoso! </h2>
