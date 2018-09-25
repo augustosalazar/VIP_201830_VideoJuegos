544639382eff975af10776d0d4bdabc8869f5cad
Augusto Salazar <augustosalazar@uninorte.edu.co>
2018-09-25 18:29:07 -0500

    json parser 30%

--- ServicioWeb/php/file2bd.php
+++ ServicioWeb/php/file2bd.php
@@ -12,19 +12,53 @@ $json_a = json_decode($string, true);
 //$json_a as $key => $val
 //echo "$key => $val\n";
 
-//foreach($json_a as $key=>$value){
-//
-//        print($value);
-//
+$players = $json_a['Players'];
+
+foreach($players as $key=>$value){
+
+        print("User Id ".$value['ID']."\n");
+	print("Nivel ".$value['Nivel']."\n");
+
+	$sessions = $value['GameSessions'];
+	echo "Num de sesiones ".sizeof($sessions)."\n";
+	//echo "Las sesiones ".$sessions."\n";
+	foreach($sessions as $keyx=>$valuex){
+		print("Timestamp de session ".$valuex['TimeStamp']."\n");
+		$mini = $valuex['MiniGameSessions'];
+		echo "Num de mini sesiones ".sizeof($mini)."\n";
+		foreach($mini as $keyz=>$valuez){
+			print("TimeStampStart de mini session ".$valuez['TimeStampStart']."\n");
+			print("LevelOfAccomplishment de mini session ".$valuez['LevelOfAccomplishment']."\n");
+			//$actionEvents = $valuez['ActionEvents'];
+			//echo "Num de events ".sizeof($actionEvents)."\n";
+			//foreach($actionEvents as $keyw=>$valuew){Ã
+				//print("Type of event ".$valuew['type']."\n");
+			//	echo "hello\n";
+			//}Ã
+		}
+	}
+}
+
+//$data =  $json_a['Players'];
+//echo $data[0]['ID'] . "\n";
+
+//foreach($json_a->data as $mydata)
+//{
+//    echo $mydata->name . "\n";
+//    foreach($mydata->values as $values)
+//    {
+//        echo $values->value . "\n";
+//    }
 //}
 
-$data =  $json_a['Players'];
-echo $data[0]['ID'];
-foreach($data as $key=>$value){
+//echo "---------------------\n";
+
+
+//foreach($data as $key=>$value){
 
         //print_r($value);
 
-}
+//}
 echo "\n";
 //echo $json_a['Version de Documento'];
 //echo "\n";
@@ -33,18 +67,18 @@ echo "\n";
 //echo $json_a['Version de Parametros'];
 //echo "\n";
 
-echo "--------------\n";
+//echo "--------------\n";
 
-$jsonIterator = new RecursiveIteratorIterator(
-    new RecursiveArrayIterator(json_decode($string, TRUE)),
-    RecursiveIteratorIterator::SELF_FIRST);
-
-foreach ($jsonIterator as $key => $val) {
-    if(is_array($val)) {
-        echo "$key:\n";
-    } else {
-        echo "$key => $val\n";
-    }
-}
+//$jsonIterator = new RecursiveIteratorIterator(
+//    new RecursiveArrayIterator(json_decode($string, TRUE)),
+//    RecursiveIteratorIterator::SELF_FIRST);
+//
+//foreach ($jsonIterator as $key => $val) {
+//    if(is_array($val)) {
+//        echo "$key:\n";
+//    } else {
+//        echo "$key => $val\n";
+//    }
+//}
 
 ?>
