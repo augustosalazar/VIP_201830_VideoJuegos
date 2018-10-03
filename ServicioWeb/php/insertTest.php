0c59a96df86bd2fe8d1dfeabd2a33e74fa26f22a
Augusto Salazar <augustosalazar@uninorte.edu.co>
2018-10-03 08:22:00 -0500

    small changes

--- ServicioWeb/php/insertTest.php
+++ ServicioWeb/php/insertTest.php
@@ -15,12 +15,14 @@ $nivel = 1;
 $edad = 8;
 $insert = "insert INTO Jugador(idJugador,nombreJugador,nivelJujador,edadJugador) VALUES (". $idJugador . "," . $nombre . "," . $nivel . "," . $edad .");";
 
+$query_verify = "SELECT * FROM Jugador where idJugador= ".$idJugador;
+
 echo $insert."\n";
 
-$result = mysqli_query($link,$insert);
-//echo $result;
+$result = mysqli_query($link,$query_verify);
 
-$result = mysqli_query($link,$query);
+
+//$result = mysqli_query($link,$query);
 while ($row = mysqli_fetch_assoc($result))
 {
     echo $row['id'], PHP_EOL;
