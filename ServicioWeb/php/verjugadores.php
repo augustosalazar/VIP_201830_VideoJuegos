70986b83112135db91a96a4d233c4643fc2705db
lpotte <lpotte@uninorte.edu.co>
2018-12-04 07:10:22 -0500

    actualizado

--- ServicioWeb/php/verjugadores.php
+++ ServicioWeb/php/verjugadores.php
@@ -28,7 +28,7 @@
         $result = mysqli_query($connection,$query);
         // Añadir registros a la tabla que se creó anteriormente, desde la base de datos  
         while ($row = mysqli_fetch_row($result)){
-            echo "<tr onclick=location='versesiones.php?var=$row[0]&var2=$row[2]'>";  
+            echo "<tr onclick=location='verdetallesactividad.php?var=$row[0]&var2=$row[2]'>";  
             echo "<td align=center>".$row[0]."</td>";  
             echo "<td align=center>".$row[2]."</td>";
             echo "<td align=center>".$row[3]."</td>";
